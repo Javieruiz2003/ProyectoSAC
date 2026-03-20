08e62cfd4872ceeea1e11898244480a19959c658
rafa_cuadrado <90984220+rafacuadrado@users.noreply.github.com>
2026-03-20 05:46:42 +0100

    terminada traducción de funciones

--- src/ad5940/ADICUP3029/AD5940PortZephyr.c
+++ src/ad5940/ADICUP3029/AD5940PortZephyr.c
@@ -11,8 +11,8 @@
 static const struct gpio_dt_spec ad5940_cs =
     GPIO_DT_SPEC_GET_BY_IDX(SPI4_NODE, cs_gpios, 0);
 
-
-void AD5940_CsClr(void)
+static const struct device *rst= DEVICE_DT_GET(DT_NODELABEL(reset));
+void AD5940_CsClrZephyr(void)
 {
     if (!device_is_ready(ad5940_cs.port)) {
         return;
@@ -20,10 +20,24 @@ void AD5940_CsClr(void)
     gpio_pin_set_dt(&ad5940_cs, 1);  /* Pone CS en bajo */
 }
 
-void AD5940_CsSet(void)
+void AD5940_CsSetZephyr(void)
 {
     if (!device_is_ready(ad5940_cs.port)) {
         return;
     }
     gpio_pin_set_dt(&ad5940_cs, 0);  /* Pone CS en alto */
+}
+void AD5940_RstSet(void)
+{
+    if (!device_is_ready(rst)) {
+        return;
+    }
+    gpio_pin_set_dt(&rst, 1);  /* Pone RST en alto */
+}
+void AD5940_RstReset(void)
+{
+    if (!device_is_ready(rst)) {
+        return;
+    }
+    gpio_pin_set_dt(&rst, 1);  /* Pone RST en alto */
 }
