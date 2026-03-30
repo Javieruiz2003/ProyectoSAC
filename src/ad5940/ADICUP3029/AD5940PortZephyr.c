cf669559996d41558899957ca5733dcbd1193190
rafa_cuadrado <90984220+rafacuadrado@users.noreply.github.com>
2026-03-30 13:12:05 +0200

    funciona

---
+++ src/ad5940/ADICUP3029/AD5940PortZephyr.c
@@ -0,0 +1,43 @@
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/spi.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/drivers/gpio.h>
+
+
+/* Define el dispositivo GPIO y el pin CS */
+#define SPI4_NODE DT_NODELABEL(spi2)
+
+static const struct gpio_dt_spec ad5940_cs =
+    GPIO_DT_SPEC_GET_BY_IDX(SPI4_NODE, cs_gpios, 0);
+
+static const struct device *rst = DEVICE_DT_GET(DT_NODELABEL(gpio0));
+void AD5940_CsClrZephyr(void)
+{
+    if (!device_is_ready(ad5940_cs.port)) {
+        return;
+    }
+    gpio_pin_set_dt(&ad5940_cs, 1);  /* Pone CS en bajo */
+}
+
+void AD5940_CsSetZephyr(void)
+{
+    if (!device_is_ready(ad5940_cs.port)) {
+        return;
+    }
+    gpio_pin_set_dt(&ad5940_cs, 0);  /* Pone CS en alto */
+}
+void AD5940_RstSet(void)
+{
+    if (!device_is_ready(rst)) {
+        return;
+    }
+    gpio_pin_set(rst, 7, 1);  /* Pone RST en alto */
+}
+void AD5940_RstReset(void)
+{
+    if (!device_is_ready(rst)) {
+        return;
+    }
+    gpio_pin_set(rst, 7, 1);  /* Pone RST en alto */
+}
--- src/main.c
+++ src/main.c
@@ -2,39 +2,73 @@
 #include <zephyr/sys/printk.h>
 #include "ad5940/ad5940.h"
 #include "ad5940/BodyImpedance.h"
-
+AppBIACfg_Type *pCfg = NULL;//esto es un puntero de la dirección donde se almacenará
+//la configuración 
 int AD5940Port_Init(void);
 
+AD5940Err err;
+uint32_t bufferLectura[512];
 int main(void)
 {
-    printk("Inicio BIA\n");
+    uint32_t adiid;
+    uint32_t chipid;
+
+    printk("Programa arrancado\n");
 
     if (AD5940Port_Init() != 0) {
-        printk("Error puerto\n");
+        printk("Error inicializando puerto AD5940\n");
         return 0;
     }
+
+    printk("Puerto AD5940 inicializado\n");
 
     AD5940_RstClr();
     k_msleep(50);
     AD5940_RstSet();
     k_msleep(50);
 
-    printk("Reset hecho\n");
+    printk("Reset AD5940 hecho\n");
 
     AD5940_Initialize();
 
-    printk("Inicializando BIA...\n");
+    adiid = AD5940_ReadReg(REG_AFECON_ADIID);
+    chipid = AD5940_ReadReg(REG_AFECON_CHIPID);
+    (void)adiid;
+    (void)chipid;
+/*
+    printk("ADIID  = 0x%08X\n", adiid);
+    printk("CHIPID = 0x%08X\n", chipid);
 
-    AppBIACfg_Type *pBIACfg;
-    AppBIAGetCfg(&pBIACfg);
+    test = AD5940_ReadReg(REG_AFE_CALDATLOCK);
+    printk("CALDATLOCK antes  = 0x%08X\n", test);
 
-    AppBIAInit(0, 0);
+    AD5940_WriteReg(REG_AFE_CALDATLOCK, 0xDE87A5AF);
 
-    printk("BIA listo\n");
+    test = AD5940_ReadReg(REG_AFE_CALDATLOCK);
+    printk("CALDATLOCK despues = 0x%08X\n", test);
+
+
+    void AD5940_Main2(void);
+   
+
+    printf("Prueba");
+    AD5940_Main2();
+
+*/
+//arranque de bioimpedance
+err = AppBIAGetCfg(&pCfg);//aqui arrancamos y almacenamos en err el resultado
+//de la configuración, y en pCfg la dirección de la configuración
+if(err==AD5940ERR_OK && pCfg!=NULL){
+//comprobamos que la configuración ha sido correcta
+    int AD5940Port_Init(void);
+printk("frecuencia: %d Hz\n", (int)pCfg->SinFreq);
+}
+AppBIAInit(bufferLectura, sizeof(bufferLectura));
+AppBIACtrl(BIACTRL_START, NULL);
 
     while (1) {
-        AppBIACtrl(BIACTRL_START, 0);
-        k_msleep(1000);
+        printk("bufferLectura[0] = %d\n", bufferLectura[0]);
+        k_msleep(2000);
     }
 
     return 0;
